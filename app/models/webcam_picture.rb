20cdd8124928f0d5f47ba6533c4b959758247e45
WalterSchreppers <wschrep@wschrep-Latitude-E6520.(none)>
2012-05-09 15:32:35 +0200

    Deleted some snapshots, added a screenshot of further developed version...

--- app/models/webcam_picture.rb
+++ app/models/webcam_picture.rb
@@ -1,5 +1,5 @@
 class WebcamPicture < ActiveRecord::Base
-  attr_accessible :name, :snapshot
+  attr_accessible :name, :snapshot, :remote_snapshot_url, :snapshot_cache
   mount_uploader :snapshot, WebcamUploader
 
 end
